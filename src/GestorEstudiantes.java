457b79445196680b358383e97244736d0fb6104c
Usuario <miguegomezcasero@gmail.com>
2026-05-09 01:49:44 +0200

    Corregido el error NaN cuando un estudiante no tiene calificaciones

--- src/GestorEstudiantes.java
+++ src/GestorEstudiantes.java
@@ -10,6 +10,10 @@ public class GestorEstudiantes {
         for (int i = 0; i < estudiante.getNotas().length; i++) { // Error: índice fuera de rango
             suma += estudiante.getNotas()[i];
         }
+        if (estudiante.getNotas().length == 0) {
+            return 0.0;
+        }
+
         return suma / estudiante.getNotas().length; // Error si el array está vacío
     }
 
