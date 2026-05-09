62c8c35b5c021233c22d47aa8dcb2486f1533be9
Usuario <miguegomezcasero@gmail.com>
2026-05-09 02:07:45 +0200

    Control de seguridad para evitar NullPointerException en el Main

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,11 @@ public class Main {
 
         System.out.println("Calculando el mejor estudiante...");
         Estudiante mejor = GestorEstudiantes.encontrarMejorEstudiante(estudiantes);
-        System.out.println("El mejor estudiante es: " + mejor.getNombre()); // Posible NullPointerException
+        if (mejor != null) {
+            System.out.println("El mejor estudiante es: " + mejor.getNombre());
+        } else {
+            System.out.println("No se ha encontrado a ningún estudiante.");
+        } //Linea cambiada
 
         System.out.println("Guardando resultados...");
         GestorEstudiantes.guardarResultados(estudiantes, "resultados.txt");
